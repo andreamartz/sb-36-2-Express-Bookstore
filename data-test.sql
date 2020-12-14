c8b3ec3c6943616bbf457ada91d56c8d7140246a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-12-14 11:24:42 -0600

    Add passing tests

--- data-test.sql
+++ data-test.sql
@@ -1,7 +1,3 @@
-
-
-
-
 DROP DATABASE IF EXISTS books-test;
 
 CREATE DATABASE books-test;
@@ -20,7 +16,3 @@ CREATE TABLE books (
   title TEXT, 
   year INTEGER
 );
-
-INSERT INTO books
-  VALUES ('1234567890', 'http://a.co/aaabbbb', 'Author Name', 'english', 254, 'A Publishing Company', 'Title Goes Here', 2020),
-  ('9876543210', 'http://a.co/cccdddd', 'Author2 Name', 'english', 2, 'Random House', 'Exciting Title Goes Here', 2005)
--- tests/bookRoutes.test.js
+++ tests/bookRoutes.test.js
@@ -4,10 +4,10 @@ const db = require("../db");
 const Book = require("../models/book");
 const { DB_URI } = require("../config");
 
-let b1, b2;
+let b1, b2, bookIsbn;
 
 beforeEach(async function () {
-  // await db.query(`DROP TABLE books`);
+  await db.query(`DROP TABLE IF EXISTS books`);
   await db.query(`
     CREATE TABLE books (
       isbn TEXT PRIMARY KEY,
@@ -20,7 +20,6 @@ beforeEach(async function () {
       year INTEGER
     )
   `);
-  // await db.query("DELETE FROM books");
 
   b1 = await Book.create({
     isbn: "0123456789",
@@ -43,11 +42,13 @@ beforeEach(async function () {
     title: "Power-Up: Unlocking Hidden Math in Video Games",
     year: 2017
   });
+
+  bookIsbn = b1.isbn;
 });
 
 afterEach(async function () {
   // await db.query("DELETE FROM books");
-  await db.query(`DROP TABLE books`);
+  await db.query(`DROP TABLE IF EXISTS books`);
 });
 
 afterAll(async function () {
@@ -57,7 +58,10 @@ afterAll(async function () {
 describe("GET /books", function () {
   test("Gets all books", async function() {
     const res = await request(app).get(`/books`);
+    const books = res.body.books;
     expect(res.statusCode).toEqual(200);
+    expect(books).toHaveLength(2);
+    expect(books[0]).toHaveProperty("isbn");
     expect(res.body).toEqual({
       "books": [
         {
@@ -87,7 +91,7 @@ describe("GET /books", function () {
 
 describe("GET /books/:isbn", function () {
   test("Gets a book by isbn", async function() {
-    const res = await request(app).get(`/books/0123456789`);
+    const res = await request(app).get(`/books/${bookIsbn}`);
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
       "book": {
@@ -101,6 +105,13 @@ describe("GET /books/:isbn", function () {
         "year": 2018
       }
     });
+    expect(res.body.book).toHaveProperty("amazon_url");
+    expect(res.body.book.isbn).toBe(bookIsbn);
+  });
+
+  test("Responds with 404 if book not found", async function() {
+    const res = await request(app).get(`/books/1`);
+    expect(res.statusCode).toEqual(404);
   });
 });
 
@@ -136,6 +147,25 @@ describe("POST /books", function () {
   });
 });
 
+describe("POST /books", function () {
+  test("400 status code if language field is missing when creating new book", async function() {
+    const res = await request(app)
+      .post(`/books`)
+      .send({
+        book: {
+          isbn: '54794759',
+          amazon_url: "https://book.com",
+          author: "mctest",
+          pages: 250,
+          publisher: "Some Publisher",
+          title: "Coding Forever",
+          year: 2011
+        }
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+});
+
 describe("PUT /books/:isbn", function () {
   test("Updates a book", async function() {
     const res = await request(app)
@@ -165,6 +195,26 @@ describe("PUT /books/:isbn", function () {
         "year": 2020
       }
     });
+  });
+});
+
+describe("PUT /books/:isbn", function () {
+  test("404 when isbn of book to update does not exist", async function() {
+    const res = await request(app)
+      .put(`/books/012`)
+      .send({
+        book: {
+          isbn: "0123456789",
+          amazon_url: "http://a.co/hgfdhgf",
+          author: "Ryan Martz",
+          language: "english",
+          pages: 280,
+          publisher: "Publisher Press",
+          title: "How to Trick Any Audience",
+          year: 2020
+        }
+      });
+    expect(res.statusCode).toEqual(404);
   });
 });
 
